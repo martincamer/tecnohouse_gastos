b42be182d2459db6805c1cacc95236e4f0802b9e
martincamer <martincamer011@gmail.com>
2024-03-08 23:46:15 -0300

    Asd

--- src/components/ui/SearchSelectTipo.jsx
+++ src/components/ui/SearchSelectTipo.jsx
@@ -7,14 +7,14 @@ export const SearchSelectTipo = ({ handleTipoChange, tipoSeleccionado }) => {
         className="bg-white max-md:text-sm border-[1px] border-slate-300 text-gray-700 font-bold rounded-xl shadow-black/20 shadow-md py-3 px-3 w-[230px] placeholder:text-gray-500/90 outline-none"
       >
         <option>TODOS LOS TIPOS</option>
-        <option value="ventana">VENTANA</option>
-        <option value="puerta">PUERTA</option>
-        <option value="rajas de abrir">RAJA DE ABRIR</option>
-        <option value="paño fijo">PAÑO FIJO</option>
+        <option value="ventanas">VENTANAS</option>
+        <option value="puertas">PUERTAS</option>
+        <option value="rajas de abrir">RAJAS DE ABRIR</option>
+        <option value="paños fijo">PAÑOS FIJO</option>
         <option value="portones">PORTONES</option>
         <option value="celosias de abrir">CELOSIAS DE ABRIR</option>
         <option value="celosias corredizas">CELOSIAS CORREDIZAS</option>
-        <option value="puerta de abrir">PUERTA DE ABRIR</option>
+        <option value="puertas de abrir">PUERTAS DE ABRIR</option>
       </select>
     </div>
   );
