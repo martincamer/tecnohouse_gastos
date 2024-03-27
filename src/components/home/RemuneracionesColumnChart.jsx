6bf3adef00d7a44c113979817d13ffcefa8741bb
martincamer <martincamer011@gmail.com>
2024-03-26 22:37:56 -0300

    AS

--- src/components/home/RemuneracionesColumnChart.jsx
+++ src/components/home/RemuneracionesColumnChart.jsx
@@ -41,17 +41,11 @@ const RemuneracionesColumnChart = ({ datos }) => {
     },
     plotOptions: {
       bar: {
-        borderRadius: 5,
-        colors: {
-          ranges: [
-            {
-              from: Number.MIN_SAFE_INTEGER,
-              to: 3,
-              color: "#FF0000",
-            },
-          ],
-          backgroundBarColors: [],
-          backgroundBarOpacity: 1,
+        borderRadius: 2,
+        dataLabels: {
+          style: {
+            colors: ["#000000"],
+          },
         },
       },
     },
@@ -81,7 +75,7 @@ const RemuneracionesColumnChart = ({ datos }) => {
         series={series}
         type="bar"
         // width="1000"
-        className="w-[100%] max-md:w-[1000px]"
+        className="w-[100%] max-md:w-[1500px] "
         height={500}
       />
     </div>
